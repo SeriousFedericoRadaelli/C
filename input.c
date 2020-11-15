c585faa7c35d99425a84efe115f9ae81344ee0f5
Federico Radaelli <66851758+SeriousFedericoRadaelli@users.noreply.github.com>
2020-11-15 12:03:26 +0100

    Update input.c

--- input.c
+++ input.c
@@ -15,7 +15,7 @@ int main(){
 
     char voto;
         printf("inserisici il tuo voto: \n");
-        scanf("%c", &voto); //uguale a printf ma al contrario char
+        scanf(" %c", &voto); //uguale a printf ma al contrario char, lo spazio qui dalle virgoletto è obbligatorio 
         printf("il tuo voto è %c \n", voto);
 
     char nome [30];
